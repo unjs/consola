6c577668ed13a0340ba0ba2cddb094f527e6a361
Clark Du <clark.duxin@gmail.com>
2018-04-03 22:03:11 +0800

    refactor: add reporters entry

---
+++ src/reporters/index.js
@@ -0,0 +1,11 @@
+import BasicReporter from './basic'
+import FancyReporter from './fancy'
+import JSONReporter from './json'
+import WinstonReporter from './winston'
+
+export default {
+  BasicReporter,
+  FancyReporter,
+  JSONReporter,
+  WinstonReporter
+}
