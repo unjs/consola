90da862e6e7ad9fbadacf65f84245fca41d46933
Pooya Parsa <pyapar@gmail.com>
2018-10-08 19:50:23 +0330

    feat: improve packaging and exports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import env from 'std-env'
 import Consola from './consola'
-import Reporters from './reporters'
+import {
+  BasicReporter,
+  FancyReporter,
+  JSONReporter,
+  WinstonReporter
+} from './reporters'
 
 // Attach consola to the global to prevent
 // duplicated instances when used with different packages/versions
@@ -16,18 +21,26 @@ if (!consola || Consola.prototype.__VERSION__ !== otherVersion) {
   })
 
   if (env.minimalCLI) {
-    consola.add(new Reporters.BasicReporter())
+    consola.add(new BasicReporter())
   } else {
-    consola.add(new Reporters.FancyReporter())
+    consola.add(new FancyReporter())
   }
 
-  Object.assign(consola, { Consola }, Reporters)
+  Object.assign(consola, {
+    Consola,
+    BasicReporter,
+    FancyReporter,
+    JSONReporter,
+    WinstonReporter
+  })
 
-  global.consola = consola
+  if (typeof global !== 'undefined') {
+    global.consola = consola
+  }
 
   if (otherVersion) {
     consola.warn(
-      'Multiply versions of consola found!',
+      'Different versions of consola found!',
       `${otherVersion} replaced by ${consola.__VERSION__}`
     )
   }
--- src/reporters/index.js
+++ src/reporters/index.js
@@ -1,11 +1,4 @@
-import BasicReporter from './basic'
-import FancyReporter from './fancy'
-import JSONReporter from './json'
-import WinstonReporter from './winston'
-
-export default {
-  BasicReporter,
-  FancyReporter,
-  JSONReporter,
-  WinstonReporter
-}
+export { default as BasicReporter } from './basic'
+export { default as FancyReporter } from './fancy'
+export { default as JSONReporter } from './json'
+export { default as WinstonReporter } from './winston'
