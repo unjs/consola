c015c31f5f5b98804e15d5186820423664e4efd5
Pooya Parsa <pyapar@gmail.com>
2018-10-08 19:59:20 +0330

    feat: no more side effects

--- src/index.js
+++ src/index.js
@@ -7,43 +7,28 @@ import {
   WinstonReporter
 } from './reporters'
 
-// Attach consola to the global to prevent
-// duplicated instances when used with different packages/versions
-let consola = global && global.consola
-
-const otherVersion = consola ? consola.__VERSION__ : null
-
-if (!consola || Consola.prototype.__VERSION__ !== otherVersion) {
-  consola = new Consola({
-    level:
-      (typeof process !== 'undefined' && parseInt(process.env['CONSOLA_LEVEL'])) ||
-      (env.debug ? 4 : 3)
-  })
-
-  if (env.minimalCLI) {
-    consola.add(new BasicReporter())
-  } else {
-    consola.add(new FancyReporter())
-  }
-
-  Object.assign(consola, {
-    Consola,
-    BasicReporter,
-    FancyReporter,
-    JSONReporter,
-    WinstonReporter
-  })
-
-  if (typeof global !== 'undefined') {
-    global.consola = consola
-  }
-
-  if (otherVersion) {
-    consola.warn(
-      'Different versions of consola found!',
-      `${otherVersion} replaced by ${consola.__VERSION__}`
-    )
-  }
+// Log level
+let level = env.debug ? 4 : 3
+if (typeof process !== 'undefined' && process.env['CONSOLA_LEVEL']) {
+  level = parseInt(process.env['CONSOLA_LEVEL'])
 }
 
-export default consola
+// Create new consola instance
+const consola = new Consola({
+  level
+})
+
+// Add default reporter based on env
+if (env.minimalCLI) {
+  consola.add(new BasicReporter())
+} else {
+  consola.add(new FancyReporter())
+}
+
+Object.assign(consola, {
+  Consola,
+  BasicReporter,
+  FancyReporter,
+  JSONReporter,
+  WinstonReporter
+})
