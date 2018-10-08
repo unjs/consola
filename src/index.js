73bdd1a62ef7041bf217ffbcfefe8c0f886a6845
Pooya Parsa <pyapar@gmail.com>
2018-10-08 19:40:56 +0330

    feat: detect version changes and throw a warning

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import Reporters from './reporters'
 
 // Attach consola to the global to prevent
 // duplicated instances when used with different packages/versions
-
 let consola = global && global.consola
 
-if (!consola) {
+const otherVersion = consola ? consola.__VERSION__ : null
+
+if (!consola || Consola.prototype.__VERSION__ !== otherVersion) {
   consola = new Consola({
     level:
       (typeof process !== 'undefined' && parseInt(process.env['CONSOLA_LEVEL'])) ||
@@ -23,6 +24,13 @@ if (!consola) {
   Object.assign(consola, { Consola }, Reporters)
 
   global.consola = consola
+
+  if (otherVersion) {
+    consola.warn(
+      'Multiply versions of consola found!',
+      `${otherVersion} replaced by ${consola.__VERSION__}`
+    )
+  }
 }
 
 export default consola
