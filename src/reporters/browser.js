30cd4f0a117733217c5f4d0e0725f122715ef03e
pimlie <pimlie@hotmail.com>
2018-10-11 19:16:49 +0200

    feat: add symbols to browser

--- src/reporters/browser.js
+++ src/reporters/browser.js
@@ -4,17 +4,43 @@ export default class BrowserReporter {
   }
 
   log (logObj) {
+    /*
+     * Symbol width is dependent on user's system font
+     *
+     * In Chrome:
+     * - console.error, warn and trace have collapsable output
+     * - trace is expanded by default
+     *
+     * In Firefox:
+     * - console.error has collapsable output
+     * - console.warn has not
+     * - console.trace is always expanded and has no style support
+     *
+     * In IE:
+     * - ?
+     */
     const replaceColors = {
       undefined: 'grey',
       yellow: 'goldenrod',
       white: 'grey'
     }
+
+    // 
+   const symbols = {
+      start: '\u25cf',
+      info: '\u2139',
+      success: '\u2713',
+      debug: '\u2026',
+      log: '\u276f',
+      ready: '\u2764'
+      // warn: '\u00a0' // non-breaking space to help alignment in FF
+    }
     const color = replaceColors[logObj.color] ? replaceColors[logObj.color] : logObj.color
 
-    const styleDefault = 'color: silver; background-color: inherit;'
+    const styleDefault = 'color: ' + color + '; background-color: inherit;'
+    const styleDiscrete = 'color: silver; background-color: inherit;'
     const styleAdditional = 'color: ' + (logObj.additionalColor ? logObj.additionalColor : 'black') + '; background-color: inherit;'
     const badgeStyle = 'color: white; background-color: ' + color + ';'
-    const style = 'color: ' + color + '; background-color: inherit;'
 
     let type = logObj.type
     if (!console[type]) { // eslint-disable-line no-console
@@ -45,7 +71,12 @@ export default class BrowserReporter {
       '%c' + (logObj.args.length ? '\n' + logObj.args.join() : '')
     ]
 
-    /* eslint-disable-next-line no-console */
-    console[type](args.join(' '), badgeStyle, styleDefault, style, styleAdditional)
+    if (symbols[logObj.type]) {
+      args.unshift('%c' + symbols[logObj.type])
+      console[type](args.join(' '), styleDefault, badgeStyle, styleDiscrete, styleDefault, styleAdditional)
+    } else {
+      /* eslint-disable-next-line no-console */
+      console[type](args.join(' '), badgeStyle, styleDiscrete, styleDefault, styleAdditional)
+    }
   }
 }
