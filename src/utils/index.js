8748c81d1924aa8697f1253d8cb5d6672d1d3b8b
Pooya Parsa <pyapar@gmail.com>
2018-11-19 15:27:00 +0330

    fix(isLogObj): handle non-standard error objects

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,8 +2,22 @@ export function isPlainObject (obj) {
   return Object.prototype.toString.call(obj) === '[object Object]'
 }
 
+// TODO: remove for consola@3
 export function isLogObj (arg) {
   // Should be plain object
-  // Also contains either message or args field
-  return isPlainObject(arg) && (Boolean(arg.message || arg.args))
+  if (!isPlainObject(arg)) {
+    return false
+  }
+
+  // Should contains either 'message' or 'args' field
+  if (!arg.message && !arg.args) {
+    return false
+  }
+
+  // Handle non-standard error objects
+  if (arg.stack) {
+    return false
+  }
+
+  return true
 }
