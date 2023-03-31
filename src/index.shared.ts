8fc0fdd9bb63cb9ebbab8f6bbc4f06f8bc22f2c7
Pooya Parsa <pooya@pi0.io>
2023-03-31 17:06:28 +0200

    chore: update shared exports

--- src/index.shared.ts
+++ src/index.shared.ts
@@ -2,4 +2,3 @@ export { Consola } from "./consola";
 export { Types } from "./log.types";
 export { LogLevels } from "./log.levels";
 export { isLogObj } from "./utils";
-export { assignGlobalConsola } from "./utils/global";
