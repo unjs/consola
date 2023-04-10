b92d23b3d7364d8db65fe36682310613e399230f
Pooya Parsa <pooya@pi0.io>
2023-04-10 23:28:53 +0200

    refactor: use `index.node.ts` for main build

--- build.config.ts
+++ build.config.ts
@@ -6,7 +6,7 @@ export default defineBuildConfig({
   },
   hooks: {
     "rollup:options"(_, options) {
-      for (const output of options.output) {
+      for (const output of options.output as any[]) {
         // @ts-ignore
         output.exports = "named";
       }
