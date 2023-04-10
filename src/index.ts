6833945b435bf702fbd3696b4167554d56550c70
Pooya Parsa <pooya@pi0.io>
2023-04-10 22:54:17 +0200

    fix: pass level from `CONSOLA_LEVEL` to the `defaults` (resolves #129)

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,13 @@ export function createConsola(options: Partial<ConsolaOptions> = {}) {
   // Log level
   let level = _getDefaultLogLevel();
   if (process.env.CONSOLA_LEVEL) {
-    level = Number.parseInt(process.env.CONSOLA_LEVEL) || level;
+    level = Number.parseInt(process.env.CONSOLA_LEVEL) ?? level;
   }
 
   // Create new consola instance
   const consola = _createConsola({
     level: level as LogLevel,
+    defaults: { level },
     prompt: (...args) => import("./prompt").then((m) => m.prompt(...args)),
     reporters: options.reporters || [
       isCI || isTest ? new BasicReporter({}) : new FancyReporter({}),
