167b6f38f2f56fda15361cc72577acf2e2bdddbd
Pooya Parsa <pooya@pi0.io>
2023-04-11 11:20:50 +0200

    fix(utils): use default `stream.write` for workers support (resolves #173)

--- src/utils/stream.ts
+++ src/utils/stream.ts
@@ -1,16 +1,4 @@
-import { writeSync } from "node:fs";
-
 export function writeStream(data: any, stream: NodeJS.WriteStream) {
-  // const write = (stream as any).__write || stream.write;
-  return writeSync((stream as any).fd, data);
-  // Async
-  // return new Promise((resolve) => {
-  //   if (write.call(stream, data) === true) {
-  //     resolve(undefined);
-  //   } else {
-  //     stream.once("drain", () => {
-  //       resolve(undefined);
-  //     });
-  //   }
-  // });
+  const write = (stream as any).__write || stream.write;
+  return write.call(stream, data);
 }
