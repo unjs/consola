e6a75d4ead03afb1a2cba8e2474b0dcdd55af10d
Estéban <esteban.soubiran@insa-cvl.fr>
2023-07-01 10:27:43 +0200

    docs: remove `await` in `spinner.ts`

--- examples/spinner.ts
+++ examples/spinner.ts
@@ -1,9 +1,9 @@
 import { consola } from "./utils";
 
 async function main() {
-  await consola.start("Creating project...");
+  consola.start("Creating project...");
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  await consola.success("Project created!");
+  consola.success("Project created!");
 }
 
 main();
