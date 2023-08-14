cdc4c37b09be66b8fefea1c8961f082921a83676
13OnTheCode <13@ryongyon.com>
2023-08-14 16:37:49 +0800

    fix: path parsing on the Windows platform
    closed: #225

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,12 +1,12 @@
 import { sep } from "node:path";
 
 export function parseStack(stack: string) {
-  const cwd = process.cwd() + sep;
+  const cwd = (process.cwd() + sep).split(sep).join('/');
 
   const lines = stack
     .split("\n")
     .splice(1)
-    .map((l) => l.trim().replace("file://", "").replace(cwd, ""));
+    .map((l) => l.trim().replace(/file:\/\/\/?/, "").replace(cwd, ""));
 
   return lines;
 }
