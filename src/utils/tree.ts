77979faeef0b0c6574e41a711ece018adf260bf2
Pooya Parsa <pooya@pi0.io>
2023-12-01 12:00:25 +0100

    small refactors

--- src/utils/tree.ts
+++ src/utils/tree.ts
@@ -1,19 +1,22 @@
-import { type ColorName, getColor } from "./color";
+import { type ColorName, colorize } from "./color";
 
 export type TreeItemObject = {
   /**
    * Text of the item
    */
   text: string;
+
   /**
    * Children of the item
    */
   children?: TreeItem[];
+
   /**
    * Color of the item
    */
   color?: ColorName;
 };
+
 export type TreeItem = string | TreeItemObject;
 
 export type TreeOptions = {
@@ -21,6 +24,7 @@ export type TreeOptions = {
    * Color of the tree
    */
   color?: ColorName;
+
   /**
    * Prefix of the tree
    *
@@ -35,17 +39,16 @@ export function formatTree(items: TreeItem[], options?: TreeOptions): string {
     ...options,
   };
 
-  const tree = buildTree(items, options);
+  const tree = _buildTree(items, options).join("");
   if (options && options.color) {
-    const colorize = getColor(options.color);
-    return colorize(tree);
+    return colorize(options.color, tree);
   }
 
   return tree;
 }
 
-function buildTree(items: TreeItem[], options?: TreeOptions): string {
-  let logs = "";
+function _buildTree(items: TreeItem[], options?: TreeOptions): string[] {
+  const chunks: string[] = [];
 
   const total = items.length - 1;
   for (let i = 0; i <= total; i++) {
@@ -54,31 +57,20 @@ function buildTree(items: TreeItem[], options?: TreeOptions): string {
     const prefix = isLast ? `${options?.prefix}└─` : `${options?.prefix}├─`;
 
     if (typeof item === "string") {
-      const log = buildLog(prefix, item);
-      logs += log;
+      chunks.push(`${prefix}${item}\n`);
     } else {
-      const log = buildLog(prefix, item.text);
-      if (item.color) {
-        const colorize = getColor(item.color);
-        logs += colorize(log);
-      } else {
-        logs += log;
-      }
+      const log = `${prefix}${item.text}\n`;
+      chunks.push(item.color ? colorize(item.color, log) : log);
 
       if (item.children) {
-        const tree = buildTree(item.children, {
+        const _tree = _buildTree(item.children, {
           ...options,
           prefix: `${options?.prefix}${isLast ? "  " : "│  "}`,
         });
-
-        logs += tree;
+        chunks.push(..._tree);
       }
     }
   }
 
-  return logs;
-}
-
-function buildLog(prefix: string, text: string): string {
-  return `${prefix}${text}\n`;
+  return chunks;
 }
